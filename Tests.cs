9fd0063a3b704e4c5047ad8f3e878b37e85518b7
Andreas Stange <anst.dev@gmail.com>
2026-04-21 17:53:05 +0200

    prepared dotnet project

---
+++ Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace AiForcedAlignmentWithOnnxRuntime
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
